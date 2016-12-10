9243fe74e963aaede64d5269b3481e49d982a36d
Charles Lu <charleslu@live.com>
2016-12-10 15:28:36 -0800

    Implement general version which works for continuous case

--- particlefilter.py
+++ particlefilter.py
@@ -1,43 +1,35 @@
-#!/usr/bin/env python3
-
-from collections import Counter
 import numpy as np
 
 class ParticleFilter(object):
-    def __init__(self, state_to_true, p_particle, next_particle, n_particles=200):
+    def __init__(self, p_particles, next_particles, initial_particles):
         """
-        state_to_true: dict mapping states to true sensor reading value
-        p_particle: function where p_particle(true_reading, sensor_reading)
-            returns probability of particle
-        next_particle: function where next_particle(state, prop_param)
-            samples particle at next time step
+        p_particle: function where p_particle(states, sensor_reading)
+            returns probability of all particles
+        next_particles: function where next_particles(states, prop_param)
+            samples particles at next time step
+        initial_particles: iterable of initial particles
         """
-        self.state_to_true = state_to_true
-        self.states = list(state_to_true.keys())
-        self.p_particle = p_particle
-        self.next_particle = next_particle
-        self.n_particles = n_particles
-
-        self.particles = Counter(np.random.choice(self.states, size=self.n_particles))
+        self.p_particles = p_particles
+        self.next_particles = next_particles
+        self.particles = np.array(initial_particles)
 
     def observe(self, sensor_reading, prop_param):
         """
         Observe a sensor reading, reweight and resample particles, and then propogate in time.
         """
         # reweight
-        for state in self.particles:
-            self.particles[state] *= self.p_particle(self.state_to_true[state], sensor_reading)
+        weights = self.p_particles(self.particles, sensor_reading)
+        weights /= sum(weights)
 
-        # resample
-        total_weight = sum(self.particles.values())
-        weights = [self.particles[state] / total_weight for state in self.states]
-        resampled = np.random.choice(self.states, size=self.n_particles, p=weights)
-
-        # propogate in time
-        self.particles = Counter(self.next_particle(state, prop_param) for state in resampled)
+        # resample & propogate in time
+        self.particles = self.next_particles(
+            np.random.choice(self.particles, size=len(self.particles), p=weights),
+            prop_param
+        )
 
     def get_state_belief(self):
         """
-        Returns dict mapping state -> belief probability using current particle states.
+        Returns Counter mapping state -> belief probability using current particle states.
         """
-        return {state: self.particles[state] / self.n_particles for state in self.states}
+        return {state: count / len(self.particles)
+                for state, count in Counter(self.particles)}
