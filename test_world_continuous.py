1dcfb31f2483a0f983393ef29fdb89b98647203d
Charles Lu <charleslu@live.com>
2016-12-10 16:14:37 -0800

    Implement test continuous world

--- test_world_continuous.py
+++ test_world_continuous.py
@@ -4,48 +4,41 @@ import numpy as np
 from particlefilter import ParticleFilter
 
 def get_true_obs(state):
-    if state < 20:
-        return 5
-    elif 20 <= state < 60:
-        return state
-    elif 60 <= state < 80:
-        return 10
-    else:
-        return 20
+    return state * (50 - state) / 2 if state < 50 else (state - 50) * (100 - state)
 
 def p_particle(state, sensor_reading):
     """
     Weight using Gaussian kernel
     """
-    variance = 5
+    variance = 10
     return np.exp(-(get_true_obs(state) - sensor_reading) ** 2 / (2 * variance)) \
-        if 0 <= state < 100 else 0
+        if 0 < state < 100 else 0
 
 def next_particle(state, prop_param):
     """
     Sample new state from Gaussian around new expected location. 
     prop_param: predicted change in state
     """
-    sigma = 3
+    sigma = 4
     expected_state = state + prop_param
-    return int(np.random.normal(expected_state, sigma))
+    return np.random.normal(expected_state, sigma)
 
-n_particles = 20
+n_particles = 40
 pf = ParticleFilter(
     p_particle,
     next_particle,
-    np.random.choice(100, size=n_particles)
+    100 * np.random.random(n_particles)
 )
 
 true_state = 2
 while True:
     print('True state: {}'.format(true_state))
-    print('Particles: {}'.format(sorted(pf.particles)))
+    print('Particles: {}'.format(np.round(sorted(pf.particles), 2)))
     print('=========\n')
 
     obs = np.random.normal(get_true_obs(true_state), 5)
     print('Observation: {}'.format(obs))
-    prop_param = int(input('Enter expected change in state: '))
+    prop_param = float(input('Enter expected change in state: '))
     true_state = max(min(true_state + prop_param, 99), 0)
 
     pf.observe(obs, prop_param)
